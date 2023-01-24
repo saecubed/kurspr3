828c5e4aefd403c3f719cf92466036524b171c96
BerdnikovaVM <saecubed@gmail.com>
2023-01-24 17:22:13 +0300

    logo added

--- header.php
+++ header.php
@@ -8,19 +8,21 @@
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    
 </head>
 <body class="bg-info p-2 text-dark bg-opacity-10">
     <div class="container-fluid">   
-        <nav class="fixed-top navbar navbar-expand-lg navbar-light" style="background-color:#13085b">
-            <div class="container-fluid">
+        <nav class="fixed-top navbar navbar-expand-lg navbar-light" style="background-color:#13085b">        
+            <div class="container-fluid">                
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav text-light">
                         <li class="nav-item">
-                        <a class="nav-link text-light" href="index.php">Главная</a>
+                        <a class="nav-link text-light" href="index.php">
+                        <img src="logo.svg" height="30" class="d-inline-block align-top" alt="">
+                            Главная
+                        </a>
                         </li>
                         <li class="nav-item">
                         <a class="nav-link text-light" href="map.php">Карта</a>
