c3e8e70d56c836dcbb61adebb9d377b7a2dcda79
saecubed <106227216+saecubed@users.noreply.github.com>
2023-01-28 01:20:32 +0300

    Delete search.php

--- search.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "database.php";
-$sql = mysqli_query($conn, 'SELECT FullName, Services, Address, PublicPhone FROM `vetclinics` WHERE `FullName` LIKE '%$inputSearch%' OR `Address` LIKE '%$inputSearch%'');
-  while ($result = mysqli_fetch_array($sql)) {
-    echo '<tr>'.'<td>' .$result['FullName'].'</td>'.'<td>'. $result['Services'].'</td>'.'<td>'. $result['Address'].'</td>'.'<td>'. $result['PublicPhone'].'</td>'.'</tr>';
-  }
-?>
-
-
